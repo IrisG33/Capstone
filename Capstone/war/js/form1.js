5eafae3996214707cd447161b19b66269106684f
senthil <senthil.dexter@gmail.com>
2014-04-05 01:04:27 -0400

    js changes

--- Capstone/war/js/form1.js
+++ Capstone/war/js/form1.js
@@ -1,68 +1,154 @@
   $( document ).ready(function() {
-	/*
-		rule 1 - show "Project Number", "Building Number", "Building Entrance Number", "Unit Number" only if "Public Housing" is chosen "Program" dropdown 
-		rule 2 - show "Primary reason for correction" only if "Yes" is chosen in "Correction" radio button
-		rule 3 - show "Special Program" only if "Vouchers" is chosen in "Program" dropdown
-	*/
-	//rules for display
-	$("#rule2").hide();
-	$("#rule3").hide();
-	$("select[name='program']").on('change', function() {
-		$("#rule1").hide();
-		$("#rule3").hide();
-		if($(this).val()=="Public Housing") $("#rule1").show();
-		else if($(this).val()=="Vouchers") $("#rule3").show();
-    });
-	
-	$("input:radio[name=correction]").click(function() {
-		if($(this).val()=="Yes") $("#rule2").show();
-		else if($(this).val()=="No") $("#rule2").hide();
-    });
+	var num=1;
+	$("#removeMember").hide();
+	$( "#members" ).append(html());
+	$("#addAnotherMember").click(function() {
+		num+=1;
+		$("#removeMember").show();
+		$('#members').append(html());
+		$("input[name='totalNumberInHousehold']").prop('disabled', 'false');
+		$("input[name='totalNumberInHousehold']").val(num);
+		$("input[name='totalNumberInHousehold']").prop('disabled', 'true');
+	});
+	$("#removeMember").click(function() {
+		if(num==2)$("#removeMember").hide();
+		$('#member'+num).remove("#member"+num);
+		num-=1;
+		$("input[name='totalNumberInHousehold']").prop('disabled', 'false');
+		$("input[name='totalNumberInHousehold']").val(num);
+		$("input[name='totalNumberInHousehold']").prop('disabled', 'true');
+	});
 	
 	//form validation on submit
-	$("#form1").validate({
-		//for radio buttons
-		errorPlacement: function(error, element) {
-			if (element.attr("name") == "FSSParticipation")  { error.insertAfter("#FSSParticipationYes"); }
-			else if (element.attr("name") == "specialProgram")  { error.insertAfter("#enhancedVoucher"); }
-			else if (element.attr("name") == "primaryReasonForCorrection")  { error.insertAfter("#familyCorrectionOfIncome"); }
-			else { error.insertAfter(element); }
-		},
+	$("#form2").validate({
 		rules: {
-			agencyName: { required: true, maxlength: 10 },
-			effectiveDateOfAction: { required: true, maxlength: 10 },
-			dateOfAdmissionToProgram: { required: true, maxlength: 10 },
-			projectedEffectiveDateOfNextReexamination: { required: true, maxlength: 10 },
-			projectedDateOfNextFlatRentAnnualUpdate: { required: true, maxlength: 10 },
-			FSSParticipation: { required: true, maxlength: 10 },
-			phaCode: {
-				required: true,
-				minlength: 5, 
-				maxlength: 5
-			},
-			primaryReasonForCorrection:{ 
-				required: { depends: function(element) {return $("input:radio[name=correction]").val() == "Yes";}},
-			},
-			projectNumber: {
-				required: { depends: function(element) {return $("select[name='program']").val() == "Public Housing";}},
-			},
-			buildingNumber: {
-				required: { depends: function(element) {return $("select[name='program']").val() == "Public Housing";}},
-			},
-			buildingEntranceNumber: {
-				required: { depends: function(element) {return $("select[name='program']").val() == "Public Housing";}},
-			},
-			unitNumber: {
-				required: { depends: function(element) {return $("select[name='program']").val() == "Public Housing";}},
-			},
-			specialProgram: {
-				required: { depends: function(element) {return $("select[name='program']").val() == "Vouchers";}},
-			},
-			},
-		messages: {
-			//agencyName: "Please enter your Agency Name",
-			//for customizing error messages
-				}
+			eligibleEffectiveDate: { required: true, maxlength: 10 },
+			formerHeadOfHouseholdSSN: { required: true, maxlength: 10 },
+			lastName1: { required: true, maxlength: 10 },
+			firstName1: { required: true, maxlength: 10 },
+			MI1: { required: true, maxlength: 10 },
+			dateOfBirth1: { required: true, maxlength: 10 },
+			ageOnEffectiveDateOfAction1: { required: true, maxlength: 10 },
+			ssn1:{ required: true, maxlength: 10 } 
+			}
 	});
-
+	
+	
+	
+	
+	
+function html(){
+var html="<div class=\"member\" id=\"member"+num+"\">";
+if(num!=1) html += "<h4>Member "+num+"</h4>";
+else html += "<h4>Head of Household</h4>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Last Name<\/div>";
+html += "<div class=\"right\"><input type=\"text\" name=\"lastName"+num+"\"><\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">First Name<\/div>";
+html += "<div class=\"right\"><input type=\"text\" name=\"firstName"+num+"\"><\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">MI<\/div>";
+html += "<div class=\"right\"><input type=\"text\" name=\"MI"+num+"\"><\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Date of birth<\/div>";
+html += "<div class=\"right\">";
+html += "<input type=\"text\" name=\"dateOfBirth"+num+"\" class=\"datepicker\">";
+html += "<\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Age on Effective Date of Action<\/div>";
+html += "<div class=\"right\"><input type=\"text\" name=\"ageOnEffectiveDateOfAction"+num+"\"><\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Sex<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"sex"+num+"\">";
+html += "  <option value=\"Male\">Male<\/option>";
+html += "  <option value=\"Female\">Female<\/option>";
+html += "  <option value=\"Cannot reveal\">Cannot reveal<\/option>";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Relation<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"relation"+num+"\">";
+html += "  <option value=\"Head\">Head<\/option>";
+html += "  <option value=\"Spouse\">Spouse<\/option>";
+html += "  <option value=\"Co-head\">Co-head<\/option>";
+html += "  <option value=\"Foster child\/Foster Adult\">Foster child\/Foster Adult<\/option>";
+html += "   <option value=\"Other youth under 18\">Other youth under 18<\/option>";
+html += "  <option value=\"Full-time student 18+\">Full-time student 18+<\/option>";
+html += "  <option value=\"Live-in aide\">Live-in aide<\/option>";
+html += "  <option value=\"Other adult\">Other adult<\/option>";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Citizenship<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"citizenship"+num+"\">";
+html += "  <option value=\"Eligible citizen\">Eligible citizen<\/option>";
+html += "  <option value=\"Eligible noncitizen\">Eligible noncitizen<\/option>";
+html += "  <option value=\"Ineligible noncitizen\">Ineligible noncitizen<\/option>";
+html += "  <option value=\"Pending verification\">Pending verification<\/option>";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Disability<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"disability"+num+"\">";
+html += "  <option value=\"Yes\">Yes<\/option>";
+html += "  <option value=\"No\">No<\/option> ";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Race<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"race"+num+"\">";
+html += "  <option value=\"White\">White<\/option>";
+html += "  <option value=\"Black\/African American\">Black\/African American<\/option>";
+html += "  <option value=\"American Indian\/Alaska Native\">American Indian\/Alaska Native<\/option>";
+html += "  <option value=\"Asian\">Asian<\/option>";
+html += "  <option value=\"Native Hawaiian\/Other Pacific Islander\">Native Hawaiian\/Other Pacific Islander<\/option>  ";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Ethnicity<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"ethnicity"+num+"\">";
+html += "  <option value=\"Hispanic or Latino\">Hispanic or Latino<\/option>";
+html += "  <option value=\"Not Hispanic or Latino\">Not Hispanic or Latino<\/option>";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">SSN<\/div>";
+html += "<div class=\"right\"><input type=\"text\" name=\"ssn"+num+"\"><\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Alien Registration Number<\/div>";
+html += "<div class=\"right\"><input type=\"text\" name=\"alienRegistrationNum"+num+"\"><\/div>";
+html += "<\/div>";
+html += "<div class=\"element\">";
+html += "<div class=\"left\">Meeting Community Service or Self Sufficiency Requirement<\/div>";
+html += "<div class=\"right\">";
+html += "<select name=\"meetingCommunityServiceOrSelfSufficiencyRequirement"+num+"\">";
+html += "  <option value=\"Yes\">Yes<\/option>";
+html += "  <option value=\"No\">No<\/option> ";
+html += "<\/select>";
+html += "<\/div>";
+html += "<\/div>";
+html += "<\/div>";
+return html;
+}
 });
