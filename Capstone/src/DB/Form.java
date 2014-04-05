ba4326c43163c06b3d8d25f43e75814f1f384b32
senthil <senthil.dexter@gmail.com>
2014-04-05 14:07:28 -0400

    oracle changes

--- Capstone/src/DB/Form.java
+++ Capstone/src/DB/Form.java
@@ -35,7 +35,6 @@ public class Form {
     public boolean form2(HttpServletRequest request){
         try{
                 ArrayList<TableColumns> tc= new ArrayList<TableColumns>();
-                tc.add(new TableColumns("String", request.getParameter("HPAD_AGENCY_SNO")));
                 tc.add(new TableColumns("String", request.getParameter("HPAD_PHA_CODE")));
                 tc.add(new TableColumns("String", request.getParameter("HPAD_AGENCY_NAME")));
                 //set the Table name here
@@ -44,6 +43,7 @@ public class Form {
                 
                 ArrayList<TableColumns> tc2= new ArrayList<TableColumns>();    
                 tc2.add(new TableColumns("String", request.getParameter("HPTBD_PHA_CODE")));
+                tc2.add(new TableColumns("Integer", request.getParameter("HPTBD_SNO_HOUSEHOLD")));
                 tc2.add(new TableColumns("String", request.getParameter("HPTBD_PRGM")));
                 tc2.add(new TableColumns("String", request.getParameter("HPTBD_PROJECT_NUM")));
                 tc2.add(new TableColumns("String", request.getParameter("HPTBD_BLD_NUM")));
@@ -52,18 +52,18 @@ public class Form {
                 int HPTBD_SNO_HOUSEHOLD = new Insert().goAutoIncrement("HUD_PHA_TENANT_BLD_DTLS", tc2);
                 
                 ArrayList<TableColumns> tc3= new ArrayList<TableColumns>();    
-                tc3.add(new TableColumns("String", request.getParameter("HPAC_ACTION_TYPE")));
+                tc3.add(new TableColumns("Integer", request.getParameter("HPAC_ACTION_TYPE")));
                 int HPAC_ACTION_SNO = new Insert().goAutoIncrement("HUD_PHA_ACTION_CODE", tc3);
                 
                 ArrayList<TableColumns> tc4= new ArrayList<TableColumns>();    
                 tc4.add(new TableColumns("Integer", Integer.toString(HPAC_ACTION_SNO)));
                 tc4.add(new TableColumns("Integer", Integer.toString(HPTBD_SNO_HOUSEHOLD)));
-                tc4.add(new TableColumns("String", request.getParameter("HTPA_EFFECTIVE_DATE")));
+                tc4.add(new TableColumns("Date", request.getParameter("HTPA_EFFECTIVE_DATE")));
                 tc4.add(new TableColumns("String", request.getParameter("HTPA_IS_CORRECTED")));
                 tc4.add(new TableColumns("String", request.getParameter("HTPA_IF_CORRECTED")));
-                tc4.add(new TableColumns("String", request.getParameter("HTPA_ADMISSION_DATE")));
-                tc4.add(new TableColumns("String", request.getParameter("HTPA_PROJEFFECTIVE_DATE_REEXAM")));
-                tc4.add(new TableColumns("String", request.getParameter("HTPA_PROJ_DATE_FLATRATE_UPDATE")));
+                tc4.add(new TableColumns("Date", request.getParameter("HTPA_ADMISSION_DATE")));
+                tc4.add(new TableColumns("Date", request.getParameter("HTPA_PROJEFFECTIVE_DATE_REEXAM")));
+                tc4.add(new TableColumns("Date", request.getParameter("HTPA_PROJ_DATE_FLATRATE_UPDATE")));
                 tc4.add(new TableColumns("String", request.getParameter("HTPA_FSS_PARTICIPATION")));
                 tc4.add(new TableColumns("String", request.getParameter("HTPA_SPECIAL_PROGRAMS")));
                 tc4.add(new TableColumns("String", request.getParameter("HTPA_OTHER_SPECIAL_PROGRAMS_1")));
