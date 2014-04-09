bf04aa85276c5a9499296d85448cdb3f94a03359
StevenChen@CMU <stevenchen@cmu.edu>
2014-04-09 11:15:55 -0400

    modified p5.html and p5.js

--- Capstone/build/web/js/p5.js
+++ Capstone/build/web/js/p5.js
@@ -1,7 +1,13 @@
-function addIncomeRow()
-{ 
-  $('#IncomeTable').append('<tr><td style="word-wrap:break-word;"><input size = "21" type="text" name="familyMemberName"></td><td style="word-wrap:break-word;"><input size = "2" type="text" name="no"></td><td><input size = "13" type="text" name="incomdeCode"></td><td><input size = "21" type="text" name="calculation"></td><td><input size = "13" type="text" name="dollarsPerYear" placeholder ="$"></td><td><input size = "20" type="text" name="incomeExclusions" placeholder ="$"></td><td><input size = "24" type="text" name="incomeAfterExclusions" placeholder ="$"></td></tr>');
-}
+$( document ).ready(function() {
+	var num = 0;
+	function addIncomeRow()
+	{ 	num += 1;
+		$('#IncomeTable').append('<tr><td style="word-wrap:break-word;"><input size = "21" type="text" name="familyMemberName').append(num).append('"></td><td style="word-wrap:break-word;"><input size = "2" type="text" name="no"').append(num).append('></td><td><input size = "13" type="text" name="incomdeCode"').append(num).append('></td><td><input size = "21" type="text" name="calculation"').append(num).append('></td><td><input size = "13" type="text" name="dollarsPerYear"').append(num).append(' placeholder ="$"></td><td><input size = "20" type="text" name="incomeExclusions"').append(num).append(' placeholder ="$"></td><td><input size = "24" type="text" name="incomeAfterExclusions"').append(num).append(' placeholder ="$"></td></tr>');
+	}
+
+
+});
+
 function removeIncomeRow()
 { 
   $('#IncomeTable tr:last').remove();
